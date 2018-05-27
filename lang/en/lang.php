8ff707343f32262ed9218086b2f521f3f33779e5
frthjf <lifeworld-api@posteo.net>
2018-05-27 12:43:38 +0100

    Consistent plugin description

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,6 @@
 <?php return [
     'plugin' => [
-        'name' => 'Form Store',
-        'description' => ' Flexible frontend form manager to collect and store information to a database, e.g. online applications, surveys and much more.'
+        'name' => 'FormStore',
+        'description' => 'Flexible frontend form manager to collect and store information using database models'
     ]
 ];
